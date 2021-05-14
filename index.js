661e3f6d162a1f5dd07d140e4d86fff5303af4ab
abc <abc@example.com>
2021-05-14 16:59:29 +0530

    make it better

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.set('view engine', 'ejs');
 app.get('/',(req,res)=>{
     res.render('index');
 });
-app.listen(process.env.PORT | 5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
     console.log('Website running on localhost 5000');
 })
